3c510ae3c0f6b47a722339174f5053d941a77b7d
Michelle Joudrey <michelle-joudrey@users.noreply.github.com>
2016-10-07 16:10:17 -0400

    Initial commit

---
+++ MyExtensionApp/MyExtension/SourceEditorExtension.swift
@@ -0,0 +1,26 @@
+//
+//  SourceEditorExtension.swift
+//  MyExtension
+//
+//  Created by Michelle Joudrey on 10/7/16.
+//  Copyright © 2016 Michelle Joudrey. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorExtension: NSObject, XCSourceEditorExtension {
+    
+
+    func extensionDidFinishLaunching() {
+        // If your extension needs to do any work at launch, implement this optional method.
+    }
+    
+    /*
+    var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
+        // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
+        return []
+    }
+    */
+    
+}
---
+++ MyExtensionApp/MyExtensionApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyExtensionApp
+//
+//  Created by Michelle Joudrey on 10/7/16.
+//  Copyright © 2016 Michelle Joudrey. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
