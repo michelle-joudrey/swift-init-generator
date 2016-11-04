293b465daf0623568ba17f381f474ea0777c01e7
Michelle Joudrey <michelle-joudrey@users.noreply.github.com>
2016-11-03 20:26:35 -0400

    Handle indentation better

--- MyExtensionApp/MyExtension/SourceEditorCommand.swift
+++ MyExtensionApp/MyExtension/SourceEditorCommand.swift
@@ -21,15 +21,25 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
         }
         let selectedCode = buffer[buffer.range(for: TextRange(position: selection))!]
         let selectedVars = vars(inCode: selectedCode)
-        let initParams = selectedVars.map { "\($0.name): \($0.type)" }.joined(separator: ", ")        
-        let indent: String = {
+        let initParams = selectedVars.map { "\($0.name): \($0.type)" }.joined(separator: ", ")
+        let indentString: String = {
             if invocation.buffer.usesTabsForIndentation {
                 return "\t"
             }
             return String(repeating: " ", count: invocation.buffer.tabWidth)
         }()
-        let initBody = selectedVars.map { "\(indent)self.\($0.name) = \($0.name)" }.joined(separator: "\n")
-        let generatedInit = "init(\(initParams)) {\n\(initBody)\n}".components(separatedBy: "\n").map { indent + $0 }.joined(separator: "\n")
+        let indentBase: String = {
+            let firstLine = invocation.buffer.lines[selection.start.line] as! String
+            guard let firstNonWhiteSpaceIndex = firstLine.characters.index(where: {
+                let scalar = String($0).unicodeScalars.first!
+                return !CharacterSet.whitespaces.contains(scalar)
+            }) else {
+                return indentString // first selected line contains no whitespace, there's no indentation hint
+            }
+            return firstLine[firstLine.startIndex ..< firstNonWhiteSpaceIndex]
+        }()
+        let initBody = selectedVars.map { "\(indentString)self.\($0.name) = \($0.name)" }.joined(separator: "\n")
+        let generatedInit = "init(\(initParams)) {\n\(initBody)\n}".components(separatedBy: "\n").map { indentBase + $0 }.joined(separator: "\n")
         if selection.end.line == invocation.buffer.lines.count - 1 {
             invocation.buffer.lines.add(generatedInit)
         }
